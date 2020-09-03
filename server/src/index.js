10ea736b132550a1fed8d90b306e53a1d1ee54c3
soosietwo <susiekim9@gmail.com>
2020-09-03 17:45:41 -0400

    Create nominee query and mutation

--- server/src/index.js
+++ server/src/index.js
@@ -1,27 +1,19 @@
-const { GraphQLServer } = require('graphql-yoga')
-const { prisma } = require('./generated/prisma-client')
-const Query = require('./resolvers/Query')
-const Mutation = require('./resolvers/Mutation')
-const Subscription = require('./resolvers/Subscription')
-const User = require('./resolvers/User')
-const Link = require('./resolvers/Link')
-const Vote = require('./resolvers/Vote')
+const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
 
 const resolvers = {
   Query,
   Mutation,
-  Subscription,
-  User,
-  Link,
-  Vote,
-}
+};
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers,
-  context: request => ({
+  context: (request) => ({
     ...request,
     prisma,
   }),
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,32 +1,8 @@
-async function feed(parent, args, context) {
-  const count = await context.prisma
-    .linksConnection({
-      where: {
-        OR: [
-          { description_contains: args.filter },
-          { url_contains: args.filter },
-        ],
-      },
-    })
-    .aggregate()
-    .count()
-  const links = await context.prisma.links({
-    where: {
-      OR: [
-        { description_contains: args.filter },
-        { url_contains: args.filter },
-      ],
-    },
-    skip: args.skip,
-    first: args.first,
-    orderBy: args.orderBy,
-  })
-  return {
-    count,
-    links,
-  }
+async function nominees(parent, args, context) {
+  const nominees = await context.prisma.nominees();
+  return nominees;
 }
 
 module.exports = {
-  feed,
-}
+  nominees,
+};
