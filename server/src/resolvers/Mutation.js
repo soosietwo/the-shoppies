2743c0572d5c8d3b4bc00eb133c996785279519e
soosietwo <susiekim9@gmail.com>
2020-09-07 16:55:54 -0400

    Delete all nominees

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -8,7 +8,12 @@ const deleteNominee = (parent, args, context, info) => {
   return context.prisma.deleteNominee({ id: args.id });
 };
 
+const deleteAllNominees = (parent, args, context) => {
+  return context.prisma.deleteManyNominees({ id_not: "undefined" });
+};
+
 module.exports = {
   addNominee,
   deleteNominee,
+  deleteAllNominees,
 };
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,38 @@
-import React from "react";
-import { Sheet, Heading, Button, Scrollable } from "@shopify/polaris";
+import React, { useState } from "react";
+import { Sheet, Heading, Button, Scrollable, Toast } from "@shopify/polaris";
 import { MobileCancelMajorMonotone } from "@shopify/polaris-icons";
+import { gql, useMutation } from "@apollo/client";
 
 import MoviesList from "./MoviesList";
 import NomineeCard from "./NomineeCard";
+import { NOMINEES_QUERY } from "./Home";
+import { NOMINEES_CONNECTION_QUERY } from "./Header";
+
+const DELETE_ALL_NOMINEES_MUTATION = gql`
+  mutation DELETE_ALL_NOMINEES_MUTATION {
+    deleteAllNominees {
+      count
+    }
+  }
+`;
 
 const Sidebar = ({ toggleSheetActive, sheetActive, nominees }) => {
+  const [showToast, setShowToast] = useState(false);
+
+  const [deleteAllNominees, { loading, data, error }] = useMutation(
+    DELETE_ALL_NOMINEES_MUTATION,
+    {
+      refetchQueries: [
+        { query: NOMINEES_QUERY },
+        { query: NOMINEES_CONNECTION_QUERY },
+      ],
+      update(cache, { data, error }) {
+        if (error) return console.error(error);
+        if (data) setShowToast(true);
+      },
+    }
+  );
+
   return (
     <Sheet open={sheetActive}>
       <div
@@ -51,12 +78,20 @@ const Sidebar = ({ toggleSheetActive, sheetActive, nominees }) => {
             width: "100%",
           }}
         >
-          <Button onClick={toggleSheetActive}>Cancel</Button>
-          <Button primary onClick={toggleSheetActive}>
+          <Button loading={loading} onClick={toggleSheetActive}>
+            Cancel
+          </Button>
+          <Button loading={loading} primary onClick={deleteAllNominees}>
             Submit Nominees
           </Button>
         </div>
       </div>
+      {showToast ? (
+        <Toast
+          content="Nominees Submitted!"
+          onDismiss={() => setShowToast(false)}
+        />
+      ) : null}
     </Sheet>
   );
 };
