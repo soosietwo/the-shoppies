8a1c80b52d9b922964f1fe17b8e1ebb66954d308
soosietwo <susiekim9@gmail.com>
2020-10-21 21:04:44 -0400

    Create query resolver for movies

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,15 +1,42 @@
+const axios = require("axios");
+
+const movies = async (parent, args) => {
+  const { searchTerm, page } = args;
+  const url = "https://www.omdbapi.com";
+
+  return axios
+    .get(url, {
+      params: {
+        apikey: process.env.API_KEY,
+        type: "movie",
+        s: searchTerm.trim(),
+        page,
+      },
+    })
+    .then((res) => {
+      const movies = res.data.Search.map((movie) => ({
+        title: movie.Title,
+        year: movie.Year,
+        id: movie.imdbID,
+        poster: movie.Poster,
+      }));
+
+      return { movies, totalResults: res.data.totalResults };
+    })
+    .catch((err) => console.log(err.response.data.Error));
+};
+
 const nominees = async (parent, args, context) => {
   return await context.prisma.nominees();
 };
 
 const nomineesConnection = async (parent, args, context) => {
   const aggregate = await context.prisma.nomineesConnection().aggregate();
-  const pageInfo = await context.prisma.nomineesConnection().pageInfo();
-  const edges = await context.prisma.nomineesConnection().edges();
-  return { aggregate, pageInfo, edges };
+  return { aggregate };
 };
 
 module.exports = {
+  movies,
   nominees,
   nomineesConnection,
 };
